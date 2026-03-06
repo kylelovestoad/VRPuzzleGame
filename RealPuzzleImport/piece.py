93b64c8c8b50d5a9451447cddcc215a6326be9dc
joe <josephfielding03@gmail.com>
2026-03-06 15:19:03 -0500

    #28 piece caves and hull sections

--- RealPuzzleImport/piece.py
+++ RealPuzzleImport/piece.py
@@ -1,28 +1,31 @@
+# Credit: https://agniva.me/scipy/2016/10/25/contour-smoothing.html
+from dataclasses import dataclass
+
+import cv2
 import numpy as np
 from scipy.interpolate import make_splprep
 
 
 NUM_PIECE_POINTS = 1024
+MIN_CAVE_DEPTH = 10
 
 
 class Piece:
-    def __init__(self, spline):
-        # need more fields later
+    def __init__(self, spline, piece_hull_sections, piece_caves):
         self.spline = spline
+        self.piece_hull_sections = piece_hull_sections
+        self.piece_caves = piece_caves
 
     def as_contour(self):
-        param = np.linspace(0, 1, NUM_PIECE_POINTS)
-        x, y = self.spline(param)
-
-        res_array = [[[int(xi), int(yi)]] for xi, yi in zip(x, y)]
-        return np.asarray(res_array, dtype=np.int32)
+        return self.spline.contour
 
 
 class PieceSpline:
-    def __init__(self, spline, smooth_param, noisy_param):
+    def __init__(self, spline, smooth_param, noisy_param, contour):
         self.spline = spline
         self.smoothed_params = smooth_param
         self.noisy_param = noisy_param
+        self.contour = contour
 
     def __call__(self, *args):
         params = np.interp(*args, self.smoothed_params, self.noisy_param)
@@ -30,7 +33,20 @@ class PieceSpline:
         return self.spline(params)
 
 
-def smooth_parameter_mapping(spline):
+@dataclass
+class HullSection:
+    start_idx: int
+    end_idx: int
+
+
+@dataclass
+class CaveSection:
+    start_idx: int
+    end_idx: int
+    deepest_idx: int
+
+
+def _smooth_parameter_mapping(spline):
     noisy_params = np.linspace(0, 1, NUM_PIECE_POINTS)
     x, y = spline(noisy_params)
 
@@ -46,17 +62,72 @@ def smooth_parameter_mapping(spline):
     return smoothed_params, noisy_params
 
 
-# https://agniva.me/scipy/2016/10/25/contour-smoothing.html
-def piece_from_contour(contour):
+def _spline_contour(spline):
+    param = np.linspace(0, 1, NUM_PIECE_POINTS)
+    x, y = spline(param)
+
+    res_array = [[[int(xi), int(yi)]] for xi, yi in zip(x, y)]
+    return np.asarray(res_array, dtype=np.int32)
+
+
+def _get_piece_spline(contour):
     x = contour[:, 0, 0].tolist()
     x.append(x[0])
     y = contour[:, 0, 1].tolist()
     y.append(y[0])
     contour_points = len(x)
 
-    spline, u = make_splprep([x, y], s=contour_points)
+    spline, _ = make_splprep([x, y], s=contour_points)
+    smoothed_params, fixed_params = _smooth_parameter_mapping(spline)
+    contour = _spline_contour(spline)
 
-    smoothed_params, fixed_params = smooth_parameter_mapping(spline)
-    piece_spline = PieceSpline(spline, smoothed_params, fixed_params)
+    piece_spline = PieceSpline(spline, smoothed_params, fixed_params, contour)
 
-    return Piece(piece_spline)
+    return piece_spline
+
+
+def _get_piece_caves(convex_defects):
+    piece_caves = []
+
+    for cave in convex_defects:
+        start_idx, end_idx, deepest_idx, scaled_depth = cave[0]
+        depth = scaled_depth / 256.0
+
+        if depth > MIN_CAVE_DEPTH:
+            cave_section = CaveSection(start_idx, end_idx, deepest_idx)
+            piece_caves.append(cave_section)
+
+    return piece_caves
+
+
+def _get_piece_hull_sections(piece_caves):
+    cave_count = len(piece_caves)
+    piece_hull_sections = []
+
+    for i in range(cave_count):
+        prev_cave = piece_caves[i or cave_count - 1]
+        curr_cave = piece_caves[i]
+
+        hull_section = HullSection(prev_cave.end_idx, curr_cave.start_idx)
+        piece_hull_sections.append(hull_section)
+
+    return piece_hull_sections
+
+
+def _get_piece_sections(piece_spline):
+    contour = piece_spline.contour
+
+    convex_hull = cv2.convexHull(contour, returnPoints=False)
+    convex_defects = cv2.convexityDefects(contour, convex_hull)
+
+    piece_caves = _get_piece_caves(convex_defects)
+    piece_hull_sections = _get_piece_hull_sections(piece_caves)
+
+    return piece_hull_sections, piece_caves
+
+
+def piece_from_contour(contour):
+    piece_spline = _get_piece_spline(contour)
+    piece_hull_sections, piece_caves = _get_piece_sections(piece_spline)
+
+    return Piece(piece_spline, piece_hull_sections, piece_caves)
