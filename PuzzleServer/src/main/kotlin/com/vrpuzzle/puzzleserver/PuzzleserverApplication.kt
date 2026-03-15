c1a1d010d3e393e49c78a10d1cd011d0ddc69095
kylelovestoad <kylelovestoad+github@protonmail.com>
2026-03-15 18:35:15 -0400

    Initialize puzzle server.

---
+++ PuzzleServer/src/main/kotlin/com/vrpuzzle/puzzleserver/PuzzleserverApplication.kt
@@ -0,0 +1,11 @@
+package com.vrpuzzle.puzzleserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PuzzleserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<PuzzleserverApplication>(*args)
+}
---
+++ PuzzleServer/src/test/kotlin/com/vrpuzzle/puzzleserver/PuzzleserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vrpuzzle.puzzleserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PuzzleserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
